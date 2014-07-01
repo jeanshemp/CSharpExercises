2f18d42a609537dc9b88bb663ffd4af4298ba8bd
Jean Carlos <sheemps@gmail.com>
2014-07-01 00:11:55 -0300

    ExercicioSete

---
+++ exercicioSete/exercicioSete/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioSete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Escreva um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+            int valorDigitado;
+
+            Console.Write("Digite um valor:");
+            valorDigitado = int.Parse(Console.ReadLine());
+
+            if (valorDigitado >= 0){
+                Console.Write ("Esse valor é positivo");
+            }
+
+            else if (valorDigitado < 0)
+            {
+                Console.Write("Esse valor é negativo!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
