fa7852b365a7b7f69516823ae2b1988f32b037b5
Jean Carlos <sheemps@gmail.com>
2014-06-30 23:41:25 -0300

    Revert "Revert "Lista de Exercicios Resolvidos""
    This reverts commit 685ef7a61f78ad05d177318151c05b40e249b182.

---
+++ exercicioSeis/exercicioSeis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioSeis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//escreva um programa que receba dois numeros e imprima o maior deles
+            int maiorNumero;
+            int primeiroNumero;
+            int segundoNumero;
+            int numeroAtual;
+
+            Console.Write("Informe o primeiro Numero : ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+            Console.Write("Informe o segundo Numero : ");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+            if (primeiroNumero > segundoNumero)
+            {
+                Console.Write("O Primeiro numero é maior que o Segundo.");
+               
+            }
+
+            else if (segundoNumero > primeiroNumero)
+            {
+                Console.Write("O Segundo numero é maior que  o Primeiro.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
